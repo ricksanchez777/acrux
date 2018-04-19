15f5660588ce5a746862775d8aa04fadd11e6672
Maria Yu <aiquny@codeaurora.org>
2018-04-19 14:31:06 +0800

    sched: Allow __sched_setscheduler call from interrupt context if not pi
    BUG() is only needed for pi case which needed
    call to a rtmutex function and grab a spin_lock.
    While in pi case is a simplified logic to normalize
    tasks, it is safe to be called from interrupt
    context.
    
    CRs-Fixed: 2101157
    Change-Id: Icdcb9356692fc30d7b5af40f816381df5b0f0d58
    Signed-off-by: Maria Yu <aiquny@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Jago Gardiner (nysadev) <jagogardiner@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4257,8 +4257,9 @@ static int __sched_setscheduler(struct task_struct *p,
 	int reset_on_fork;
 	int queue_flags = DEQUEUE_SAVE | DEQUEUE_MOVE;
 
-	/* The pi code expects interrupts enabled */
-	BUG_ON(pi && in_interrupt());
+	/* may grab non-irq protected spin_locks */
+	if (pi)
+		BUG_ON(in_interrupt());
 recheck:
 	/* double check policy once rq lock held */
 	if (policy < 0) {
