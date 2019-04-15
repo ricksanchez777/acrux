26c738e847b938401c4d41c4017013b667c26bd0
Sultan Alsawaf <sultan@kerneltoast.com>
2019-04-14 22:04:53 -0700

    msm: mdss: Power on display asynchronously as early as possible
    Currently, mdss powers on the display a long time after an unblank is
    requested and gets completely blocked while powering on the display, so
    if the display takes a very long time to turn on, then mdss will be
    stuck unable to do anything else in the meantime. This results in a long
    delay between trying to wake the device and the display actually
    powering on.
    
    In order to make the display turn on faster when waking the device from
    sleep, start powering on the display as soon as the framebuffer unblank
    event is received. This allows mdss to continue resuming while the
    display takes its time powering on. A high-priority kthread is used here
    to ensure the display powers on as quickly as possible.
    
    In the event that the framebuffer unblank notifier is not used (such as
    for AOD), the display will be powered on at the time that it is
    requested via the MDSS_EVENT_LINK_READY event.
    
    To make this work, kickoffs need to be blocked when they attempt to
    power on the display, so that a kickoff won't continue while the display
    is still powered off.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Jago Gardiner (nysadev) <jagogardiner@gmail.com>

--- drivers/video/fbdev/msm/mdss_dsi.h
+++ drivers/video/fbdev/msm/mdss_dsi.h
@@ -677,6 +677,47 @@ struct mdss_dsi_ctrl_pdata {
 	bool update_phy_timing; /* flag to recalculate PHY timings */
 
 	bool phy_power_off;
+
+	bool dsi_cmd_hs;
+
+	int disp_err_fg_gpio;
+	struct delayed_work err_fg_handler;
+
+	int disp_err_detect_gpio;
+	struct delayed_work err_int_work;
+	bool rdy_err_detect;
+	bool err_detect_irq_en;
+
+	/* alpm brightness setting */
+	struct dsi_panel_cmds alpm_mode_cmds[ALPM_MODE_MAX];
+	enum alpm_mode_type alpm_mode;
+	u32 alpm_bl_threshold;
+	u32 alpm_dim_threshold;
+
+	/* rgb calibration */
+	struct dsi_cmd_pos rgb_gain_pos;
+	struct rgb_gain rgb_gain;
+
+	struct notifier_block wake_notif;
+	struct task_struct *wake_thread;
+	struct completion wake_comp;
+	wait_queue_head_t wake_waitq;
+	atomic_t disp_en;
+};
+
+enum {
+	MDSS_DISPLAY_OFF,
+	MDSS_DISPLAY_WAKING,
+	MDSS_DISPLAY_ON
+};
+
+struct te_data {
+	bool irq_enabled;
+	bool err_fg;
+	int irq;
+	unsigned long ts_vsync;
+	unsigned long ts_last_check;
+	spinlock_t spinlock;
 };
 
 struct dsi_status_data {
